09428abb94c211de7ed5d69cf5881052b4b9bc29
Carp Marcel <ycarp32@yahoo.com>
2024-07-02 16:19:09 +0300

    LongerVideo

--- JS/video.js
+++ JS/video.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector('.video-container').style.display = 'none';
         // document.body.style.overflow = 'auto'; // Enable scrolling on the body
       }, { once: true }); // Ensure the event listener is only triggered once
-    }, 1000); // 4 seconds before starting the fade-out
+    }, 15000); // 4 seconds before starting the fade-out
   });
   
